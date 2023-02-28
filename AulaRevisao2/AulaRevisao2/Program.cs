6c4730217a7f407504f1364b70b591ffd4cb74bd
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-02-28 08:57:27 -0300

    Add files via upload
    Terceira revisão

---
+++ AulaRevisao2/AulaRevisao2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+static void Soma()
+{
+    int n1 = 1;
+    int n2 = 2;
+    int n3 = 3;
+
+    int soma = n1 + n2 + n3;
+
+    Console.WriteLine(soma);
+}
+
+Soma();
+*/
+
+/*
+static int Cubo(int l)
+{
+    return l * l * l;
+}
+
+Console.WriteLine(Cubo(2));
+*/
+
+static int F1 (int x)
+{
+    return x * 5;
+}
+
+static int F2(int x)
+{
+    return x * 2;
+}
+
+Console.WriteLine(F2(F1(2)));
